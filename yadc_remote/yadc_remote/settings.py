97633574ac64af249cd5cc0a78da3c2dc3908355
lethosor <lethosor@gmail.com>
2014-12-25 16:09:27 -0500

    Allow static files to be served properly with DEBUG=False

--- yadc_remote/yadc_remote/settings.py
+++ yadc_remote/yadc_remote/settings.py
@@ -8,27 +8,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 import yadc.util
 SECRET_KEY = yadc.util.get_secret_key()
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('YADC_DEBUG', False))
+ALLOWED_HOSTS = ('0.0.0.0', 'localhost', '127.0.0.1', '*')
 
 TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -36,7 +25,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -52,10 +40,6 @@ ROOT_URLCONF = 'yadc_remote.urls'
 
 WSGI_APPLICATION = 'yadc_remote.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -63,25 +47,10 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(yadc.util.abspath('yadc_remote/static')),
-)
+STATIC_PATH = yadc.util.abspath('yadc_remote/static')
--- yadc_remote/yadc_remote/urls.py
+++ yadc_remote/yadc_remote/urls.py
@@ -1,12 +1,14 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'yadc_remote.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_PATH,
+        'show_indexes': settings.DEBUG,
+    }),
 )
